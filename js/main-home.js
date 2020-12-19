e96fcf6f4045039a5f6f68675fe6096a4a346cc7
LauraKo <kovarskyte@yahoo.com>
2020-12-19 14:12:57 +0200

    Counter css+js

--- js/main-home.js
+++ js/main-home.js
@@ -28,6 +28,26 @@ aboutCardsRender('.testclass1', '.testclass2', aboutCardsData)
 
 /* about video */
 /* counter */
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount, 1);
+        } else {
+            count.innerText = target;
+        }
+    }
+    updateCount();
+});
+
 /* services */
 /* projects */
 /* news */
