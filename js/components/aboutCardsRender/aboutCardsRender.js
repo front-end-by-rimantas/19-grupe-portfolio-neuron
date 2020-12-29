948eb25264f3eac55b44c5f4420cf1dad09e38c6
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-12-29 09:53:48 +0200

    remeove comments from about cards render

--- js/components/aboutCardsRender/aboutCardsRender.js
+++ js/components/aboutCardsRender/aboutCardsRender.js
@@ -11,7 +11,7 @@ function aboutCardsRender(selector1, selector2, aboutCardsData) {
     const DOM2 = document.querySelector(selector2);
     let HTML1 = '';
     let HTML2 = '';
-    // console.log(DOM1, DOM2)
+    console.log(DOM1, DOM2)
     const aboutCount = aboutCardsData.data.length;  //
     for(let i = 0; i < aboutCount/2; i++) {
         const aboutCard = aboutCardsData.data[i]; // 
@@ -31,7 +31,7 @@ function aboutCardsRender(selector1, selector2, aboutCardsData) {
                 
                 </div>`
             }
-            // DOM1.innerHTML = HTML1;
-            // DOM2.innerHTML = HTML2;
+            DOM1.innerHTML = HTML1;
+            DOM2.innerHTML = HTML2;
         }
 export { aboutCardsRender }
